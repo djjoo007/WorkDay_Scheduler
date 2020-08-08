18a1ccfaeb6f0157f462f6768bdf928aa5fce25b
Joo Heredia <djjoo007uw@gmail.com>
2020-08-07 21:57:24 -0700

    add pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# homework5
+# Workday_Scheduler
+
+The Daily Planner displays the current date at top of calendar.
+Time Blocks are shown for standard business hours 9:00am - 5:00pm.
+Each timeblock is color coded to indicate whether t he even is in the past, present and future.
+Events can be added in the textfield and save button will store event in local storage.
+
+Utilize:
+DOM
+moment.js
+localStorage
--- script.js
+++ script.js
@@ -3,35 +3,54 @@ $(document).ready(function () {
 
     // Onclick event for saveBtn
     $('saveBtn').on('click', function () {
-        var value = $(this).siblings('.descrption').val();
+        let value = $(this).siblings('.descrption').val();
 
-        var time = $(this).parent().attr('id');
+        let time = $(this).parent().attr('id');
 
         localStorage.setItem(time, value);
     });
 
-    function timeHighLight () {
+    function timeHighlight() {
         // display current time with moment
-        var currentTime = moment().hour();
+        let currentTime = moment().hour();
     
         $('.time-block').each(function() {
 
-            var blockTime = parseInt($(this).attr('id').split('-')[1]);
+            let blockTime = parseInt($(this).attr('id').split('-')[1]);
 
             if (blockTime < currentTime) {
                 $(this).addClass('past');
                 
+            } else if (blockTime === currentTime) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
             }
-
-            
-
-        })
-
-
-
-
+        });
     }
 
+    // Time stored in local storage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+      
+
+
+    // Callback function
+    timeHighlight();
+    setInterval(timeHighlight, 1500);
+
+    // format time
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
 
 
@@ -42,6 +61,4 @@ $(document).ready(function () {
 
 
 
-
-
-})
+});
